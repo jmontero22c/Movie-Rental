fa91e77ab1cb12cbecb5a72cb9c1ca90427c1253
jmontero22c <jesusda71.2@gmail.com>
2022-08-30 09:38:54 -0500

    Initial commit

---
+++ src/main/java/beans/Alquiler.java
@@ -0,0 +1,70 @@
+package beans;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author jesus
+ */
+public class Alquiler {
+    private int id;
+    private int username;
+    private Date fechaAlquiler;
+    private boolean novedad;
+    private String genero;
+
+    public Alquiler(int id, int username, Date fechaAlquiler, boolean novedad, String genero) {
+        this.id = id;
+        this.username = username;
+        this.fechaAlquiler = fechaAlquiler;
+        this.novedad = novedad;
+        this.genero = genero;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUsername() {
+        return username;
+    }
+
+    public void setUsername(int username) {
+        this.username = username;
+    }
+
+    public Date getFechaAlquiler() {
+        return fechaAlquiler;
+    }
+
+    public void setFechaAlquiler(Date fechaAlquiler) {
+        this.fechaAlquiler = fechaAlquiler;
+    }
+
+    public boolean isNovedad() {
+        return novedad;
+    }
+
+    public void setNovedad(boolean novedad) {
+        this.novedad = novedad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    @Override
+    public String toString() {
+        return "Alquiler{" + "id=" + id + ", username=" + username + ", fechaAlquiler=" + fechaAlquiler + ", novedad=" + novedad + ", genero=" + genero + '}';
+    }
+    
+    
+}
